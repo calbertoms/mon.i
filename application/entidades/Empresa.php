55dcff924e6d62f73d441cd1eab9d1928fb4b8e2
Carlos Alberto <>
2017-09-29 18:59:42 -0300

    Criada Classe Empresa.

---
+++ application/entidades/Empresa.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Empresa
+ *
+ * @author Suporte
+ */
+class Empresa {
+    
+    private $id;
+     
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function cadastrar(){
+        
+    }
+     
+     
+}
