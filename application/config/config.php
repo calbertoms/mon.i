3aab7969a8b169ac4e9bd2e0b45f203021cd43c6
Carlos Alberto <Suporte@192.168.254.196>
2018-04-08 15:59:35 -0300

    MODIFICADO MONITORINTELIGENTE.PHP

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.43.50/mon.i';
+$config['base_url'] = 'http://192.168.0.200/mon.i';
 
 /*
 |--------------------------------------------------------------------------
